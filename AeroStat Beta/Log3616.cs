e444262ee528152a662564aa6ffe5a0f1079ca11
Shane DeSouza <shane.desouza.1@us.af.mil>
2022-09-19 10:07:27 -0600

    Made this cs from a block of code!

---
+++ AeroStat Beta/Log3616.cs
@@ -0,0 +1,20 @@
+namespace AeroStat_Beta
+{
+    public class Log3616
+    {
+        public long id { get; set; }
+        public long shiftid { get; set; }
+        public long certifierid { get; set; }
+        public long atlasid { get; set; }
+        public DateTime originalentrytime { get; set; }
+        public string originalopinitials { get; set; }
+        public bool archive { get; set; }
+        public string archiveby { get; set; }
+        public DateTime archivetime { get; set; }
+        public long reviewinitials { get; set; }
+        public DateTime entrytime { get; set; }
+        public string entry { get; set; }
+        public string initials { get; set; }
+        public string callback { get; set; }
+    }
+}
